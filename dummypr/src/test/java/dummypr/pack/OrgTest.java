71a6497f1058380543cfacffc7ece434b8f030e3
Nishant-TE <nishantviplove@gmail.com>
2025-06-30 15:35:34 +0530

    changes

---
+++ dummypr/src/test/java/dummypr/pack/OrgTest.java
@@ -0,0 +1,16 @@
+package dummypr.pack;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println(" execute createOrgTest");
+	}
+
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("cxecute modifyOrgTest");
+		
+	}
+}
