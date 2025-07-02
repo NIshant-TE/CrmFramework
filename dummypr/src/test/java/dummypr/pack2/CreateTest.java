8f3f3f1deef3d424e6304eaef42e39221ca900ea
NIshant-TE <nishantviplove@gmail.com>
2025-07-02 11:08:02 +0530

    jen kin dummy

---
+++ dummypr/src/test/java/dummypr/pack2/CreateTest.java
@@ -0,0 +1,18 @@
+package dummypr.pack2;
+import org.testng.annotations.Test;
+
+public class CreateTest {
+
+		@Test
+		public void createOrg1Test() {
+			System.out.println(" execute createOrgTest2");
+		}
+
+		@Test
+		public void modifyOrg1Test() {
+			System.out.println("cxecute modifyOrgTest2");
+			
+		}
+	}
+
+
